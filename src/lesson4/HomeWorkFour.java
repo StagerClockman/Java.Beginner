0c9279df708cf86fe3210162a551310389f6a42b
Pavel.Stolyarov <Pavel.Stolyarov@nexign.com>
2021-11-15 19:55:15 +0700

    Added HomeWorkFour

--- src/lesson4/HomeWorkFour.java
+++ src/lesson4/HomeWorkFour.java
@@ -4,14 +4,13 @@ package src.lesson4;
  * Java1.HomeWorkFour
  *
  * @author Pavel.Stolyarov
- * @version 14.11.2021
+ * @version 15.11.2021
  */
 import java.util.Random;
 import java.util.Scanner;
 
 public class HomeWorkFour {
     static int SIZE = 3;
-    static int SIGN_TO_WIN = 3;
     static final char SIGN_X = 'x';
     static final char SIGN_O = 'o';
     static final char SIGN_EMPTY = '.';
@@ -19,7 +18,7 @@ public class HomeWorkFour {
     static Scanner sc = new Scanner(System.in);
     static Random rnd = new Random();
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         initTable();
         printTable();
         while (true) {
@@ -46,21 +45,32 @@ public class HomeWorkFour {
         }
         System.out.println("GAME OVER");
     }
-    public static boolean checkWin(char symb) {
-        if(table[0][0] == symb && table[0][1] == symb && table[0][2] == symb) return true;
-        if(table[1][0] == symb && table[1][1] == symb && table[1][2] == symb) return true;
-        if (table[2][0] == symb && table[2][1] == symb && table[2][2] == symb) return true;
-        if (table[0][0] == symb && table[1][0] == symb && table[2][0] == symb) return true;
-        if (table[0][1] == symb && table[1][1] == symb && table[2][1] == symb) return true;
-        if (table[0][2] == symb && table[1][2] == symb && table[2][2] == symb) return true;
-        if (table[0][0] == symb && table[1][1] == symb && table[2][2] == symb) return true;
-        if (table[2][0] == symb && table[1][1] == symb && table[0][2] == symb) return true;
+    public static boolean checkWin(char ch) {
+        int count_h;
+        int count_v;
+        for (int i = 0; i < SIZE; i++) {
+            count_h = 0;
+            count_v = 0;
+            for (int j = 0; j < SIZE; j++) {
+                if (table[i][j] == ch) {
+                    count_h++;
+                }
+                if (table[j][i] == ch) {
+                    count_v++;
+                }
+            }
+            if (count_h == SIZE || count_v == SIZE) {
+                return true;
+            }
+        }
+        if (table[0][0] == ch && table[1][1] == ch && table[2][2] == ch) return true;
+        if (table[2][0] == ch && table[1][1] == ch && table[0][2] == ch) return true;
         return false;
     }
     public static boolean isTableFull() {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                if (table[i][j] == SIGN_TO_WIN) return false;
+                if (table[i][j] == SIGN_EMPTY) return false;
             }
         }
         return true;
@@ -71,16 +81,16 @@ public class HomeWorkFour {
             x = rnd.nextInt(SIZE);
             y = rnd.nextInt(SIZE);
         } while (!isCellValid(x, y));
-        System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
+        System.out.println("AI move " + (x + 1) + " " + (y + 1));
         table[y][x] = SIGN_O;
     }
     public static void humanTurn() {
         int x, y;
         do {
-            System.out.println("Введите координаты в формате X Y");
+            System.out.println("Print X Y");
             x = sc.nextInt() - 1;
             y = sc.nextInt() - 1;
-        } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
+        } while (!isCellValid(x, y));
         table[y][x] = SIGN_X;
     }
     public static boolean isCellValid(int x, int y) {
